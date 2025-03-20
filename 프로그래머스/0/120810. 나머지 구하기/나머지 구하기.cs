3d4b62af2b6722f6838c9ae48077041f3127274c
Ji Hoon Yeo <devhoon1999@gmail.com>
2025-03-21 02:13:49 +0900

    [level 0] Title: 나머지 구하기, Time: 0.30 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cs
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.cs
@@ -2,13 +2,17 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        bool isValid = (0 < num1 && num1 <= 100) || (0 < num2 && num2 <= 100);
-        if (isValid)
+        int answer;
+        bool isValid = (0 < num1 && num1 <= 100) && (0 < num2 && num2 <= 100);
+        if (!isValid) 
         {
-            answer = num1 % num2;
-            return answer;
+            throw new ArgumentOutOfRangeException("num1, num2는 0보다 크고 100 이하이어야 합니다.");
         }
-        throw new Exception("num1, num2 는 0보다 크고 100보다 작거나 같아야 합니다.");
+        if (num2 == 0) 
+        {
+            throw new DivideByZeroException("num2는 0이 될 수 없습니다.");
+        }
+        answer = num1 % num2;
+        return answer;
     }
 }
