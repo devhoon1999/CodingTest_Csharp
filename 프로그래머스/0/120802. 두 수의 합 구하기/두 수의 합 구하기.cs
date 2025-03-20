676020734a47f651406ae986635b5c94bab9317e
Ji Hoon Yeo <devhoon1999@gmail.com>
2025-03-21 01:37:42 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.27 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cs
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.cs
@@ -2,7 +2,13 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = num1 + num2 ;
-        return answer;
+        int answer = 0;
+        bool isValid = (num1 >= -50000 && num1 <= 50000) && (num2 >= -50000 && num2 <= 50000);
+        if (isValid) 
+        {
+            answer = num1 + num2;
+            return answer;
+        }
+        throw new ArgumentOutOfRangeException("num1과 num2는 -50000 이상 -50000 이하의 값이어야 합니다.");
     }
 }
